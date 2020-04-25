a23b71914b5e94051c98fac0b6be6edb784aa301
Diko Consunji <d.consunji@arcanys.com>
2020-04-26 00:50:21 +0800

    created sample lambda + layers build.

---
+++ aws/cloudformation/lambda/handler/hello-world/index.js
@@ -0,0 +1,16 @@
+
+function handler(event, context, callback) {
+  console.log(
+    "Event: ",
+    JSON.stringify(event, null, 3)
+  );
+
+  callback(
+    null,
+    {
+      message: "Ok"
+    }
+  );
+}
+
+exports.handler = handler;
