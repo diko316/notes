9ef0176f231d081b0eaeddc5f3f9f4e9a0df8e6f
Diko Consunji <d.consunji@arcanys.com>
2020-04-26 22:00:19 +0800

    Completed creation of cloudformation deployment shell script with basic features.

--- aws/cloudformation/lambda/lambda/hello-world/index.js
+++ aws/cloudformation/lambda/lambda/hello-world/index.js
@@ -1,8 +1,17 @@
-
 function handler(event, context, callback) {
   console.log(
     "Event: ",
     JSON.stringify(event, null, 3)
+  );
+
+  console.log(
+    "Package1: ",
+    require("@test-layer/package1")
+  );
+
+  console.log(
+    "Package2: ",
+    require("@test-layer/package2")
   );
 
   callback(
